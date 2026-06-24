dd42bfa4824e9c59c23f58bfed7627339d3bce21
diegocosta23 <baraweanton@gmail.com>
2026-06-24 12:23:58 +0200

    Update SnapperShot

--- src/snappershot/app/application.py
+++ src/snappershot/app/application.py
@@ -11,9 +11,14 @@ from src.snappershot.ui.styles import STYLE
 def run() -> None:
     app = QApplication(sys.argv)
 
+    app.setApplicationName("SnapperShot")
     app.setStyleSheet(STYLE)
 
     window = MainWindow()
     window.show()
 
     sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    run()
